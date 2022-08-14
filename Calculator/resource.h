3c4466537631117e840437fa13affb182573f6ef
teo9974 <teo9974@naver.com>
2022-08-14 18:12:27 +0900

    계산기 만들기

---
+++ Calculator/resource.h
@@ -0,0 +1,46 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Calculator.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_CALCULATOR_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_CALCULATOR                  107
+#define IDI_SMALL                       108
+#define IDC_CALCULATOR                  109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_BUTTON1                     1000
+#define IDC_EDIT1                       1002
+#define IDC_BUTTON2                     1006
+#define IDC_BUTTON3                     1007
+#define IDC_BUTTON4                     1023
+#define IDC_BUTTON5                     1024
+#define IDC_BUTTON6                     1025
+#define IDC_BUTTON7                     1026
+#define IDC_BUTTON8                     1027
+#define IDC_BUTTON9                     1028
+#define IDC_BUTTON10                    1040
+#define IDC_BUTTON11                    1041
+#define IDC_BUTTON12                    1042
+#define IDC_BUTTON13                    1043
+#define IDC_BUTTON14                    1044
+#define IDC_BUTTON15                    1045
+#define IDC_BUTTON16                    1046
+#define IDC_BUTTON17                    1047
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
--- imageDlg/imageDlg.cpp
+++ imageDlg/imageDlg.cpp
@@ -1,8 +1,17 @@
 ﻿#include "framework.h"
 #include "imageDlg.h"
+
 #include <gdiplus.h>
 #pragma comment(lib, "gdiplus")
 using namespace Gdiplus;
+
+int flag = 0;
+int num1 = 0, num2 = 0, result = 0;
+
+void NumBtn(HWND hWnd, int num)
+{
+    SetDlgItemInt(hWnd, IDC_EDIT1, 1, TRUE);
+}
 
 // 정보 대화 상자의 메시지 처리기입니다.
 INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
@@ -19,7 +28,7 @@ INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hDlg, &ps);
         Graphics gp(hdc);
-        Image img = TEXT("img1.jpg");
+        Image img =L"img1.jpg";
         gp.DrawImage(&img, 0, 0, 800, 600);
         EndPaint(hDlg, &ps);
     }
@@ -29,10 +38,14 @@ INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
 
     case WM_COMMAND:
+        int id = LOWORD(wParam);
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
         {
             EndDialog(hDlg, LOWORD(wParam));
             return (INT_PTR)TRUE;
+        }
+        else if (id == IDC_BUTTON1) {
+            NumBtn(hDig, 1);
         }
         break;
     }
